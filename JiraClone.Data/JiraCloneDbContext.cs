28aa5aa78384a9373c3688a90095053f275a5ce6
lenam2809 <49664798+lenam2809@users.noreply.github.com>
2024-01-04 19:17:20 +0700

    Add project files.

---
+++ JiraClone.Data/JiraCloneDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraClone.Data
+{
+    public partial class JiraCloneDbContext : DbContext 
+    {
+        public JiraCloneDbContext()
+        {
+        }
+
+        public JiraCloneDbContext(DbContextOptions<JiraCloneDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
